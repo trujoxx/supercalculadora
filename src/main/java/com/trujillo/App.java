72141dd28c776e2585bf6c37f1ae13125a105f78
JavierM640 <fmaroje640@g.educaand.es>
2022-02-07 13:23:32 +0100

    Creación del Menú y Submenú

--- src/main/java/com/trujillo/App.java
+++ src/main/java/com/trujillo/App.java
@@ -1,5 +1,7 @@
 package com.trujillo;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,49 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Scanner s = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+
+        while(!salir){
+            System.out.println("1.Área de polígonos regulares y teorema de Pitágoras");
+            System.out.println("2.Operaciones aritméticas");
+            System.out.println("3.Equación de 1º y 2º grado y grado superior 2 a Ruffini");
+            System.out.println("0. Salir");
+
+            System.out.println("Introduce una opción: ");
+            opcion = s.nextInt();
+
+            switch (opcion){
+                case 1:
+                System.out.println("1.Triángulo");
+                System.out.println("2.Circulo");
+                System.out.println("3.Pentágono");
+                System.out.println("4.Rectángulo");
+                System.out.println("Escoge una operación:");
+                opcion = s.nextInt();
+                    switch(opcion){
+                        case 1:
+                        poligonos.triangulo();
+                        break;
+                        case 2:
+                        poligonos.circulo();
+                        break;
+                        case 3:
+                        poligonos.pentagono();
+                        break;
+                        case 4:
+                        poligonos.rectangulo();
+                        break;
+                        default:
+                    }
+                break;
+                case 2:
+
+                case 0:
+                salir = true;
+            }
+
+        } 
     }
 }
