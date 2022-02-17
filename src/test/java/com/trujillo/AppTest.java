554e093b5bfb4ece580c92943e56b30e4eb77380
JavierM640 <fmaroje640@g.educaand.es>
2022-02-17 08:58:15 +0100

    Creacion de Pruebas Unitarias

--- src/test/java/com/trujillo/AppTest.java
+++ src/test/java/com/trujillo/AppTest.java
@@ -1,8 +1,17 @@
 package com.trujillo;
 
-import static org.junit.Assert.assertTrue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 
 /**
  * Unit test for simple App.
@@ -12,9 +21,42 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @BeforeEach
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
     }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @Test
+    @DisplayName("Test entrada a Menú de Aritmética, opción A")
+    public void testCasoMenuAritmetica() {
+       
+        provideInput("A\n1\n10\n33\nX\nX");
+        
+        App.main(new String[0]);
+        assertThat(getOutput(), containsString("Resultado: 43") );
+        
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
 }
